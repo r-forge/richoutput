4497b4636dcc073d0caa80222b27781ef8f34181
altermattw (Dr. ) <altermattw@hanover.edu>
2011-08-29 12:10:06 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/richoutput@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/DeducerRichOutput/R/print.ez.R
+++ pkg/DeducerRichOutput/R/print.ez.R
@@ -4,8 +4,9 @@ print.ez <- function(results) {
 	for(i in 1:length(results)) {
 			code = paste(code,"<h3>",names(results[i]),"</h3>\n",sep="")
 			code = paste(code,h.df(results[[i]], rowcolors = TRUE),"\n",sep="")
+			if(!is.null(attr(results[i],"warnings"))) code = paste(code,attr(results[i],"warnings"))
 		}
-	if(!is.null(attr(results,"warnings"))) code = paste(code,attr(results,"warnings"))
+#	if(!is.null(attr(results,"warnings"))) code = paste(code,attr(results,"warnings"))
 	code = paste(code,"<pre>\n")
 	cat(code)
 	invisible(results)
