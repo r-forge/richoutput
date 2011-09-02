84137c40c3bbfec6704ab74af038d460129ab212
altermattw (Dr. ) <altermattw@hanover.edu>
2011-09-02 00:44:37 +0000

    removing gg2way2 experimental function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/richoutput@53 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/DeducerANOVA/R/gg2way.R
+++ pkg/DeducerANOVA/R/gg2way.R
@@ -20,32 +20,3 @@ gg2way <- function(data,exclude,x,y,xwithin = FALSE, split = NULL,x_lab = NULL, 
 		cmd = paste(cmd,"\nprint(g)")
 		execute(cmd)	
 		}
-
-gg2way2 <- function(data,exclude,x,y,xwithin = FALSE, split = NULL,x_lab = NULL, y_lab = NULL, split_lab = NULL)
-	{
-		if(is.null(split)) split = "NULL"
-		if(length(split) == 0) split = "NULL"
-		if(!is.null(exclude)) data=data[-exclude,]
-		xvar <- data[,match(x,names(data)]
-		yvar <- data[,match(y,names(data)]
-		splitvar <- data[,match(split,names(data)]
-		dev.new()
-		g <- ggplot() + theme_bw() +
-		geom_errorbar(aes(y = yvar, x = xvar, linetype = splitvar, group = splitvar),
-			data=data, width = 0.4, fun.data = 
-			x = data[,match(x,names(data)], 
-			mean_cl_normal, conf.int = 0.95, stat = 'summary'
-			"data=",data,",width = 0.4, fun.data = ,",
-			"position = position_dodge(width = 0.5))+\n",
-		 "geom_point(aes(x = ", x, ", y = ",y, ", shape = ",split, ", group = ",split,"),",
-			"data=",data,",size = 3.0, fun.data = mean_cl_normal, conf.int = 0.95,stat = 'summary', position = position_dodge(width = 0.5))",sep="")
-		if(xwithin) {
-			cmd = paste(cmd,"+\ngeom_line(aes(x = ", x, ", y = ",y, ", linetype = ",split, ", group = ",split,"),",
-			"data=",data,", fun.data = mean_cl_normal, conf.int = 0.95,stat = 'summary', position = position_dodge(width = 0.5))",sep="")
-				}
-		if(!is.null(x_lab)) cmd = paste(cmd,"+\nscale_x_discrete(name = \"",x_lab,"\")",sep="")
-		if(!is.null(y_lab)) cmd = paste(cmd,"+\nscale_y_continuous(name = \"",y_lab,"\")",sep="")  
-		if(!is.null(split_lab)) cmd = paste(cmd,"+\nscale_linetype(name = \"",split_lab,"\") + scale_shape(name = \"",split_lab,"\")",sep="") 
-		cmd = paste(cmd,"\nprint(g)")
-		execute(cmd)	
-		}
