aa30e23160f3741810ef43e665b53c5df3b7cc82
altermattw (Dr. ) <altermattw@hanover.edu>
2012-09-06 16:42:24 +0000

    Fixing onLoad, adding data.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/richoutput@127 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/DeducerAlpha/R/onLoad.R
+++ pkg/DeducerAlpha/R/onLoad.R
@@ -1,15 +1,15 @@
 .onLoad <- function(libname, pkgname) { 
 	if(.deducer == .jnull())
 		return(NULL)
-	.jpackage(pkgname,lib.loc=libname) 	#loads ReshapeDialog.jar
+	.jpackage(pkgname,lib.loc=libname) 	#loads jar file
 
 	AlphaD <<- J("DeducerAlpha.Alpha")
 
 	deducer.addMenuItem("Cronbach Alpha",,".getAlphaDialog()$run()","Analysis")
 	if(.windowsGUI){			
-		winMenuAddItem("Analysis", "Cronbach Alpha", "deducer('Cronbach Alpha')")
+		winMenuAddItem("Analysis", "Cronbach's Alpha", "deducer('Cronbach Alpha')")
 	}else if(.jgr){	
 		jgr.addMenuSeparator("Analysis")		
-		jgr.addMenuItem("Analysis", "Cronbach Alpha", "deducer('Cronbach Alpha')")
+		jgr.addMenuItem("Analysis", "Cronbach's Alpha", "deducer('Cronbach Alpha')")
 	}
 }
