4cecbe6561620a7bba38c2750a203e4ee5252679
altermattw (Dr. ) <altermattw@hanover.edu>
2012-09-07 19:58:24 +0000

    Removing dependency for reshape.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/richoutput@129 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/DeducerANOVA/R/zzz.R
+++ pkg/DeducerANOVA/R/zzz.R
@@ -1,17 +1,14 @@
 .First.lib <- function(libname, pkgname) {
 	if(.deducer == .jnull())
 		return(NULL)
-	require(reshape)
 	require(stringr)
 	require(Hmisc)
 	.jpackage(pkgname, lib.loc = libname)
 		deducer.addMenuItem("ANOVA",,"getEZAnalysisDialog()$run()","Analysis")
-		deducer.addMenuItem("Reshape Data",,"getMeltDialog()$run()","Data")
 		if(.windowsGUI){			
 			winMenuAddItem("Analysis", "ANOVA", "deducer('ANOVA')")
 			winMenuAddItem("Data", "Reshape Data", "deducer('Reshape Data')")
 		}else if(.jgr){			
 			jgr.addMenuItem("Analysis", "ANOVA", "deducer('ANOVA')")
-			jgr.addMenuItem("Data", "Reshape Data", "deducer('Reshape Data')")
 		}
 }
