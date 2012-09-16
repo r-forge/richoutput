c6cdafef2d5344f5ef7604edf618a43843fe7edf
altermattw (Dr. ) <altermattw@hanover.edu>
2012-09-16 23:51:17 +0000

    Removing reference to Java objects from onLoad.R.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/richoutput@132 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/DeducerANOVA/R/onLoad.R
+++ pkg/DeducerANOVA/R/onLoad.R
@@ -1,9 +1,9 @@
 .onLoad <- function(libname, pkgname) {
-	if(.deducer == .jnull())
-		return(NULL)
-	.jpackage(pkgname, lib.loc = libname)
+	#if(.deducer == .jnull())
+	#	return(NULL)
+	#.jpackage(pkgname, lib.loc = libname)
 	
-	ANOVAd <<- J("DeducerANOVA.ANOVA")
+	#ANOVAd <<- J("DeducerANOVA.ANOVA")
 
 	# Setting options to permit Type-3 SS that correspond to SAS and SPSS output:	
 	options(contrasts=c("contr.sum","contr.poly"))
